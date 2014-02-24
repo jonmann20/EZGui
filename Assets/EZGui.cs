87def6eb935d97da770ba02f42b1df366286bfcd
Jon Wiedmann <jonwiedmann@gmail.com>
2014-02-23 23:33:58 -0500

    buggy btn hover

--- Assets/EZGui.cs
+++ Assets/EZGui.cs
@@ -1,6 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/*
+    A class for easily laying out text with Unity's OnGUI() function.
+    
+    1) Setup a target resolution, and layout your GUI relative to this.
+    2) Make a call to EZGui.init() at the start of your OnGUI() function.
+    3) Let EZGui handle the aspect ratio and resolution math for you.
+
+*/
+
 public class EZGUI : MonoBehaviour {
 
     public const float FULLW = 1920;
@@ -9,47 +18,119 @@ public class EZGUI : MonoBehaviour {
     public const float HALFW = FULLW / 2;
     public const float HALFH = FULLH / 2;
 
-    public static void scaleGUI() {
+    public static void init(){
         float rx = Screen.width / FULLW;
         float ry = Screen.height / FULLH;
 
         GUI.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, new Vector3(rx, ry, 1));
     }
 
-    public static void placeTxt(string str, int fontSize, float x, float y, bool black=false) {
+    public static void placeTxt(string str, int fontSize, float x, float y, Color? color=null){
         GUIContent content = new GUIContent(str);
 
         GUIStyle style = new GUIStyle();
         style.fontSize = fontSize;
-        style.normal.textColor = black ? Color.black : Color.white;
+        style.normal.textColor = color ?? Color.white;
+
+        Vector2 size = style.CalcSize(content);
+        GUI.Label(new Rect(x - size.x/2, y - size.y, size.x, size.y), content, style);
+    }
+    
+    public static string lastToolTip = " ";
+
+    public static void test(){
+        GUILayout.Button(new GUIContent ("Quit", "Button2"));
+    }
+
+    public static bool placeBtn(string str, int fontSize, float x, float y, Color? color=null, Color? hoverColor=null) {
+        GUIContent content = new GUIContent(str);
+
+        GUIStyle style = new GUIStyle();
+        style.normal.textColor = color ?? Color.white;
+        style.fontSize = fontSize;
+        
+        Vector2 size = style.CalcSize(content);
+        Rect btnPos = new Rect(x - size.x/2, y - size.y, size.x, size.y);
+        
+        Rect fixedBtnPos = new Rect(btnPos.x, btnPos.y + size.y, btnPos.width, btnPos.height);
+        if(fixedBtnPos.Contains(GUIUtility.ScreenToGUIPoint(Input.mousePosition))) {
+            style.normal.textColor = hoverColor ?? color ?? Color.white;
+        }
+
+        return GUI.Button(btnPos, content, style);
+    }
+
+    public static void blinkTxt(string str, int fontSize, float x, float y, Color? color=null){
+        GUIContent content = new GUIContent(str);
+
+        Color c = color ?? Color.white;
+        c.a = Mathf.PingPong(Time.time, 1);
+
+        GUIStyle style = new GUIStyle();
+        style.fontSize = fontSize;
+        style.normal.textColor = c;
 
         Vector2 size = style.CalcSize(content);
         GUI.Label(new Rect(x - size.x/2, y - size.y, size.x, size.y), content, style);
     }
 
-    public static bool placeBtn(string str, int fontSize, float x, float y) {
+    public static bool blinkBtn(string str, int fontSize, float x, float y, Color? color=null, Color? hoverColor=null) {
         GUIContent content = new GUIContent(str);
 
+        Color c = color ?? Color.white;
+        Color c2 = hoverColor ?? color ?? Color.white;
+        c2.a = c.a = Mathf.PingPong(Time.time, 1);
+        
         GUIStyle style = new GUIStyle();
-        style.normal.textColor = Color.white;
-        style.alignment = TextAnchor.MiddleCenter;
-        //style.wordWrap = true;
         style.fontSize = fontSize;
+        style.normal.textColor = c;
+        style.hover.textColor = c2;
 
         Vector2 size = style.CalcSize(content);
-        return GUI.Button(new Rect(x - size.x/2, y - size.y, size.x, size.y), content, style);
+        Rect btnPos = new Rect(x - size.x/2, y - size.y, size.x, size.y);
+
+        Rect fixedBtnPos = new Rect(btnPos.x, btnPos.y + size.y, btnPos.width, btnPos.height);
+        if(fixedBtnPos.Contains(GUIUtility.ScreenToGUIPoint(Input.mousePosition))) {
+            style.normal.textColor = hoverColor ?? color ?? Color.white;
+        }
+
+        return GUI.Button(btnPos, content, style);
     }
 
-    public static void blinkTxt(string str, int fontSize, float x, float y, bool black=false) {
+    public static bool flashBtn(string str, int fontSize, float x, float y, Color? color=null, Color? hoverColor=null) {
         GUIContent content = new GUIContent(str);
 
-        float f = black ? 0 : 1;
+        Color c = color ?? Color.white;
+        Color c2 = hoverColor ?? color ?? Color.white;
+        c2.a = c.a = (Time.time % 2 < 1) ? 1 : 0;
 
         GUIStyle style = new GUIStyle();
         style.fontSize = fontSize;
-        style.normal.textColor = new Color(f, f, f, Mathf.PingPong(Time.time, 1));
+        style.normal.textColor = c;
+        style.hover.textColor = c2;
 
         Vector2 size = style.CalcSize(content);
-        GUI.Label(new Rect(x - size.x/2, y - size.y, size.x, size.y), content, style);
+        Rect btnPos = new Rect(x - size.x/2, y - size.y, size.x, size.y);
+
+        Rect fixedBtnPos = new Rect(btnPos.x, btnPos.y + size.y, btnPos.width, btnPos.height);
+        if(fixedBtnPos.Contains(GUIUtility.ScreenToGUIPoint(Input.mousePosition))) {
+            style.normal.textColor = hoverColor ?? color ?? Color.white;
+        }
+
+        return GUI.Button(btnPos, content, style);
+    }
+
+    public static Rect getPos(string str, int fontSize, float x, float y){
+        GUIContent content = new GUIContent(str);
+
+        GUIStyle style = new GUIStyle();
+        style.fontSize = fontSize;
+
+        Vector2 size = style.CalcSize(content);
+        return new Rect(x - size.x/2, y - size.y, size.x, size.y);
     }
 }
+
+
+//style.alignment = TextAnchor.MiddleCenter;
+//style.wordWrap = true;
---
+++ Assets/Title.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Title : MonoBehaviour {
+
+    enum State {START, SELECT, INSTRUCTIONS};
+    State state = State.START;
+
+    int slot = 0;
+
+    void Update(){
+        if(state == State.SELECT){
+            float vert = Input.GetAxisRaw("Vertical");
+
+            if(vert > 0){           // up
+
+            }
+            else if(vert < 0){      // down
+
+            }
+        }
+    }
+
+    void OnGUI() {
+        EZGUI.init();
+
+        EZGUI.placeTxt("Game Title", 80, EZGUI.HALFW, 200, Color.yellow);
+
+        switch(state) {
+            case State.START:
+                if(EZGUI.flashBtn("Press Start", 55, EZGUI.HALFW, EZGUI.HALFH, Color.white, Color.red)) {
+                    state = State.SELECT;
+                }
+
+                break;
+            case State.SELECT:
+                if(EZGUI.placeBtn("Campaign", 55, EZGUI.HALFW, EZGUI.HALFH - 100)) {
+                    Application.LoadLevel("main");
+                }
+
+                if(EZGUI.placeBtn("Instructions", 55, EZGUI.HALFW, EZGUI.HALFH)) {
+                    state = State.INSTRUCTIONS;
+                }
+
+                if(EZGUI.placeBtn("Back", 55, EZGUI.HALFW, EZGUI.HALFH + 100, Color.white, Color.red)) {
+                    state = State.START;
+                }
+
+                break;
+            case State.INSTRUCTIONS:
+                if(EZGUI.placeBtn("Back", 55, 80, 80)) {
+                    state = State.SELECT;
+                }
+
+                EZGUI.placeTxt("The Instructions here", 55, EZGUI.HALFW, EZGUI.HALFH);
+
+                break;
+
+        }
+    }
+}
